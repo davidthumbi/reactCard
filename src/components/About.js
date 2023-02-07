eb38e2eb2f9b486906eb53213964549114db3cee
David Thumbi <davidthumbi78@gmail.com>
2023-02-07 14:14:34 +0300

    final

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className="about">
       <h3>About</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur minus sequi cupiditate, a distinctio sed. Voluptas sequi ut nostrum aliquam?</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        minus sequi cupiditate, a distinctio sed. Voluptas sequi ut nostrum
+        aliquam?
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About 
+export default About;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import profile from '../assets/profile.jpg'
 import '../assets/style.css'
-import { FaFacebookF } from "react-icons/fa";
-import { BsInstagram } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Info = () => {
   return (
     <div className="info">
-        <img src={profile} alt="" />
-        <h2>Laura Smith</h2>
-        <h3>Frontend Developer</h3>
-        <a href='youtube.com'>laurasmith website</a>
-        <div className='btn'>
-          <button className='email'><FaFacebookF />Email</button>
-          <button className='linkedin'><BsInstagram />Linkedin</button>
-        </div>
+      <img src={profile} alt="" />
+      <h2>Laura Smith</h2>
+      <h3>Frontend Developer</h3>
+      <a href="youtube.com">laurasmith website</a>
+      <div className="btn">
+        <button className="email">
+          <AiOutlineMail />
+          Email
+        </button>
+        <button className="linkedin">
+          <AiOutlineLinkedin />
+          Linkedin
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Info
